df896f710c6e0daef9080d43b77c4d547f05e45c
koder <vineet15.yadav@lge.com>
2017-03-16 14:06:26 +0530

    Adding view page

--- app.js
+++ app.js
@@ -10,11 +10,45 @@ var app = express();
 app.set("views",path.resolve(__dirname,"views"));
 app.set("view engine", "ejs");
 
+var entries = [];
+app.locals.entries = entries;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//used to log every request
+app.use(logger("dev"));
+
 app.get("/",function(request, response){
 	response.render("index");
-	response.send("Hi");
 }
 );
+
+app.get("/new-entry",function(request, response){
+	response.render("new-entry");
+}
+);
+
+app.get("/new-entry",function(request, response){
+	
+	if((!request.body.title) || (!request.body.body)){
+		response.status(400).send("Entries must have a title and a body.");
+		return;
+	}
+	entries.push({
+		title : request.body.title,
+		content : request.body.body,
+		published : new Date()
+	});
+	
+	response.redirect("/");
+}
+);
+
+app.use(function(request, response){
+	response.status(404).render("404");
+	
+});
+
 
 http.createServer(app).listen(port, function(){
 	console.log("Guestbook app started on port: "+port);
